d33289cb2050d017c1659adb01166c1def4585b5
noumannaveed <50023360+noumannaveed@users.noreply.github.com>
2021-12-17 18:13:42 +0500

    add screen

--- screens/PhoneNumber.js
+++ screens/PhoneNumber.js
@@ -24,133 +24,7 @@ const { height, width } = Dimensions.get('screen');
 
 const PhoneNumber = ({ navigation }) => {
     const [isLoading, setIsLoading] = useState(false);
-    const [notificationLoading, setNotificationLoading] = useState(false);
-    const notification = (fcmToken, data, uid, connection) => {
-        fetch('https://fcm.googleapis.com/fcm/send', {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": "key=AAAArc-UobE:APA91bEuxAzyQBJfkst1uSClNiWmre1tW5DOePJXMNFuXR7mu5a-8kl9eaMyk2tVLMGB3505YrQZN4634EdnQdW3rligTtQMRp30TsUVgwLh6VJJK-HvaMEXVLqZnNbGOT1ekitoNEPn"
-            },
-            body: JSON.stringify({
-                "to": fcmToken,
-                "notification": {
-                    "title": data.firstname + [' '] + data.lastname,
-                    "body": "Friend request!",
-                },
-                "data": {
-                    "type": "new-request",
-                    "user": [data],
-                    "uid": [uid],
-                    "connection": [connection],
-                },
-                "mutable_content": false,
-                "sound": "Tri-tone"
-            }),
-        }).then(() => {
-            console.warn('sended');
-            navigation.navigate('WaitingRoom');
-        })
-    }
-    const notificationOff = (fcmToken) => {
-        fetch('https://fcm.googleapis.com/fcm/send', {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": "key=AAAArc-UobE:APA91bEuxAzyQBJfkst1uSClNiWmre1tW5DOePJXMNFuXR7mu5a-8kl9eaMyk2tVLMGB3505YrQZN4634EdnQdW3rligTtQMRp30TsUVgwLh6VJJK-HvaMEXVLqZnNbGOT1ekitoNEPn"
-            },
-            body: JSON.stringify({
-                "to": fcmToken,
-                "notification": {
-                    "title": "No user found",
-                    "body": "Try again!",
-                },
-                "data": {
-                    "type": "no-user",
-                },
-                "mutable_content": false,
-                "sound": "Tri-tone"
-            }),
-        }).then(() => {
-            console.warn('sended');
-        })
-    }
-    function generateUUID(digits) {
-        let str = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVXZ';
-        let uuid = [];
-        for (let i = 0; i < digits; i++) {
-            uuid.push(str[Math.floor(Math.random() * str.length)]);
-        }
-        return uuid.join('');
-    }
-    const handleNotification = async () => {
-        setNotificationLoading(true);
-        let value = await AsyncStorage.getItem('uid');
-        let parse = JSON.parse(value);
-        let check = '';
-        let token = '';
-        let receiveData = '';
-        let uid = '';
-        let count = 0;
-        let connectionId = generateUUID(32);
-        let connections = '';
-        firestore()
-            .collection('Users')
-            .doc(parse.user.uid)
-            .get()
-            .then(dat => {
-                check = dat.data().interest.value;
-                token = dat.data().fcmtoken;
-                receiveData = dat.data();
-                uid = parse.user.uid;
-                connections = dat.data().connection;
-                // console.log('connections=', connections[0].receiverid);
-            });
-        firestore()
-            .collection('Users')
-            .get()
-            .then(querySnapshot => {
-                querySnapshot.forEach(documentSnapshot => {
-                    const data = documentSnapshot.data();
-                    const id = documentSnapshot.id;
-                    // console.log('connections=',connections);
-                    let isCheck = false;
-                    for (var i = 0; i < connections.length; i++) {
-                        if (id === connections[i].receiverid || id === connections[i].senderid) {
-                            isCheck = true;
-                            // console.log('recieverid=', connections[i].recieverid);
-                        }
-                    }
-                    console.log('check=', isCheck);
-                    // console.log('c=', c);
-                    if (isCheck === false) {
-                        if ("interest" in data) {
-                            // console.log('condition=', c);
-                            if (data.interest.value === check && data.fcmtoken != token && data.fcmtoken != 'null') {
-                                notification(data.fcmtoken, receiveData, uid, connectionId);
-                                count++;
-                                firestore()
-                                    .collection('Connection')
-                                    .doc(connectionId)
-                                    .set({
-                                        responded: 'false',
-                                        noofuser: count,
-                                        createdAt: new Date(),
-                                        // otheruser: '',
-                                    })
-                                    .then(() => {
-                                        console.log('Connection added!');
-                                    });
-                            }
-                        }
-                    }
-                    else if (count === 0) {
-                        notificationOff(token)
-                    }
-                });
-            });
-        setNotificationLoading(false);
-    }
+    
     const logOut = async () => {
         setIsLoading(true);
         signout()
@@ -181,7 +55,6 @@ const PhoneNumber = ({ navigation }) => {
                         <ActivityIndicator color='#FFC69B' animating={notificationLoading} />
                     ) : (
                         <Button title='Send out notification' onPress={() => navigation.navigate('Notification')} />
-                        // <Button title='Send out notification' onPress={handleNotification} />
                     )
                     }
                 </View>
