74d4216063d23f0c5c2e83ca90da43306cfcc1bf
Amir Hameed <suleman217010@gmail.com>
2021-12-17 17:06:09 +0500

    push

--- index.js
+++ index.js
@@ -5,18 +5,11 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import PushNotification from "react-native-push-notification";
 import messaging from "@react-native-firebase/messaging";
 
 messaging().setBackgroundMessageHandler(async remoteMessage => {
   console.log('Message is handled in the background!', remoteMessage);
 })
 
-PushNotification.configure({
-    onNotification: function (notification) {
-        console.log("NOTIFICATION:", notification);
-      },
-      requestPermissions: Platform.OS === 'ios'
-});
 
 AppRegistry.registerComponent(appName, () => App);
